bce9f5a86de43da8ed5adc2edabddf3d67d4ab7c
Abdifatahz <faahid44@gmail.com>
2022-07-09 13:48:15 +0300

    Added about page.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebApp.Pages;
+
+public class About : PageModel
+{
+    private readonly ILogger<About> _logger;
+    public string Fullname { get; set; }
+
+    // public About(ILogger<About> logger)
+    // {
+    //     _logger = logger;
+    // }
+
+    public void OnGet()
+    {
+        Fullname = "Abdifatah Abdilahi";
+    }
+}
+
